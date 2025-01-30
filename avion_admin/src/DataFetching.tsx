0b8bfc58fa561c5fc1a315c27e168493945301c9
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-30 22:02:56 +0500

    Update DataFetching.tsx

--- avion_admin/src/DataFetching.tsx
+++ avion_admin/src/DataFetching.tsx
@@ -4,35 +4,40 @@ import { atom } from "jotai";
 import { Product } from "../interface";
 import { client } from "./sanity/lib/client";
 import { useAtom } from "jotai";
+import { useEffect } from "react";
+
 export const data = atom<Product[]>([]);
 
 const DataFetching = () => {
   const [, setProducts] = useAtom(data);
 
-  try {
+  useEffect(() => {
     const dataFetching = async () => {
-      const query = `*[_type == "product"]{
-       name,
-       tags,
-       price,
-       stock,
-       dimensions,
-       id,
-       description,
-       discount,
-       originalPrice,
-       "categoryName": category->name,
-       "slug": slug.current,
-       "imageUrl": image.asset->url,
-       rating
-     }`;
-      const fetchedProducts: Product[] = await client.fetch(query);
-      setProducts(fetchedProducts);
+      try {
+        const query = `*[_type == "product"]{
+          name,
+          tags,
+          price,
+          stock,
+          dimensions,
+          id,
+          description,
+          discount,
+          originalPrice,
+          "categoryName": category->name,
+          "slug": slug.current,
+          "imageUrl": image.asset->url,
+          rating
+        }`;
+        const fetchedProducts: Product[] = await client.fetch(query);
+        setProducts(fetchedProducts);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
     };
+
     dataFetching();
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
+  }, [setProducts]);
 
   return null;
 };
